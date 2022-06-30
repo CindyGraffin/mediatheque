73fcd3b445d7265bc7220d4d2317dc55a61a8f93
CindyGraffin <graffincindy@gmail.com>
2022-06-30 10:08:32 +0200

    project's initialization :tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_commons/navbar/navbar.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { NotFoundComponent } from './_pages/not-found/not-found.component';
+import { HomePageComponent } from './_pages/home-page/home-page.component';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomePageComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
